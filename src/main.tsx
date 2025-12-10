de8c41ff369a3d01c62a5e5b167239341fc5b669
Damokles <damokless@protonmail.com>
2025-12-10 20:41:50 +0100

    chore init tailwindcss + heroui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {HeroUIProvider} from "@heroui/react";
 import './index.css'
-import App from './App.tsx'
+import Home from './routes/home/home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <Home />
+    </HeroUIProvider>
   </StrictMode>,
 )
---
+++ src/routes/home/home.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@heroui/react'
+
+export default function Home() {
+  return (
+    <div>
+      <Button color="primary" variant='flat'>click me</Button>
+    </div>
+  )
+}
+
