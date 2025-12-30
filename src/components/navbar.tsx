ce859fb84f123d783dbd7d2939c4d9ce5c61ff95
Damokles <damokless@protonmail.com>
2025-12-30 20:10:43 +0100

    feat : purify navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,32 +2,10 @@ import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@h
 
 export default function Header() {
     return (
-        <Navbar className='bg-transparent'>
+        <Navbar className=' flex-1 bg-transparent' maxWidth="2xl">
           <NavbarBrand>
             <p className="font-bold text-inherit">DAMOKLES</p>
           </NavbarBrand>
-          <NavbarContent className="hidden sm:flex gap-4" justify="center">
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Career
-              </Link>
-            </NavbarItem>
-            <NavbarItem isActive>
-              <Link aria-current="page" href="#">
-                skills
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Certifications
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Projects
-              </Link>
-            </NavbarItem>
-          </NavbarContent>
           <NavbarContent justify="end">
             <NavbarItem>
               <Button as={Link} color="primary" href="#" variant="flat">
