5340fc8d241a45bff2855c3574cf821c3b290231
Damokles <damokless@protonmail.com>
2026-01-20 21:10:01 +0100

    fix : better elements

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {HeroUIProvider} from "@heroui/react";
 import './index.css'
-import Home from './routes/home/home.tsx'
+import Home from './routes/home/landing.tsx'
 import Navbar from './components/navbar.tsx'
-import Footer from './components/footer.tsx';
+import Footer from './components/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeroUIProvider>
-      <Navbar />
-      <Home />
-      <Footer />
+      <main>
+        <Navbar />
+          <Home />
+        <Footer />
+      </main>
     </HeroUIProvider>
   </StrictMode>,
 )
