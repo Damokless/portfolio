f03612621105bb7a0516a8a8b477a724ac3bd8d2
Damokles <damokless@protonmail.com>
2026-03-25 18:12:11 +0100

    chore : migration to herouiv3

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
-import { HeroUIProvider } from "@heroui/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles.css"
-import Header from "./components/header.tsx"
 import About from "./pages/about.tsx"
 import Home from "./pages/home.tsx"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <HeroUIProvider>
-            <Header />
             <Home />
             <About />
-        </HeroUIProvider>
     </StrictMode>
 )
