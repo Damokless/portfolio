0f472028a491537188aa61099a7a52552bbde9da
Damokles <damokless@protonmail.com>
2026-03-29 19:13:33 +0200

    feat : adding education tab

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./styles.css"
 import Header from "./components/header.tsx"
 import About from "./pages/about.tsx"
 import Home from "./pages/home.tsx"
+import Career from "./pages/career.tsx"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Header />
         <Home />
         <About />
+        <Career />
     </StrictMode>
 )
