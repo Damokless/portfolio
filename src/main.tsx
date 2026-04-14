72c1f312218efeb1fe0f215a757a02ff8c51e3a5
Damokles <damokless@protonmail.com>
2026-04-14 18:44:08 +0200

    lint : sort import

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles.css"
+import Footer from "./components/footer.tsx"
 import Header from "./components/header.tsx"
 import About from "./pages/about.tsx"
 import Career from "./pages/career.tsx"
 import Certifications from "./pages/certifications.tsx"
 import Home from "./pages/home.tsx"
-import Stack from "./pages/stack.tsx"
 import Socials from "./pages/socials.tsx"
-import Footer from "./components/footer.tsx"
+import Stack from "./pages/stack.tsx"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
