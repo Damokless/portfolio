2e557b3eb461dd19a52d2c9603032d193ac82567
Damokles <damokless@protonmail.com>
2026-04-14 16:19:41 +0200

    fiix : geek ref added

--- src/pages/socials.tsx
+++ src/pages/socials.tsx
@@ -10,8 +10,8 @@ export default function Socials() {
         <div className="py-24 px-6 " id="about">
             <div className="max-w-6xl mx-auto">
                 <div className="mb-12 text-center">
-                    <h2 className="text-2xl font-bold text-[#DE89FF] uppercase tracking-[0.4em] mb-4">
-                        If you need more ways to connect me
+                    <h2 className="text-2xl font-bold text-[#DE89FF] uppercase tracking-[0.1em] mb-4">
+                      These are the links you're looking for
                     </h2>
                 </div>
                 <div className="flex flex-wrap items-center justify-center gap-8 mb-16">
@@ -29,14 +29,6 @@ export default function Socials() {
                         </div>
                         <span className="text-xs uppercase tracking-widest text-slate-500 font-bold group-hover:text-primary transition-colors">
                             GitHub
-                        </span>
-                    </a>
-                    <a className="group flex flex-col items-center gap-3" href="/">
-                        <div className="w-14 h-14 rounded-full border border-slate-800 flex items-center justify-center group-hover:border-primary group-hover:bg-primary transition-all group-hover:shadow-[0_0_20px_rgba(188,19,254,0.4)]">
-                            <IconBrandDiscord size={32} stroke={1} />
-                        </div>
-                        <span className="text-xs uppercase tracking-widest text-slate-500 font-bold group-hover:text-primary transition-colors">
-                            discord
                         </span>
                     </a>
                     <a className="group flex flex-col items-center gap-3" href="/">
