99bd28dbbd6de66aeef086e7b557618bb0a693d0
Damokles <damokless@protonmail.com>
2026-04-17 16:11:36 +0200

    chore : adding link to certifications

--- src/pages/certifications.tsx
+++ src/pages/certifications.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@heroui/react"
+import { Card, Link } from "@heroui/react"
 import certifications from "../assets/data/certifications.json"
 
 export default function Certifications() {
@@ -15,25 +15,30 @@ export default function Certifications() {
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 ">
                     {certifications.map((certification) => (
-                        <Card
-                            className="group p-6 rounded-xl flex flex-col items-center text-center hover:border-[#bc13fe66] hover:shadow-[0_0_30px_-10px_rgba(188,19,254,0.2)] hover:bg-[#bc13fe0d] border transition"
+                        <Link
+                            className="group w-full no-underline"
+                            href={certification.url}
                             key={certification.name}>
-                            <div className="w-16 h-16 mb-4 flex items-center justify-center bg-primary/5 rounded-full border border-primary/20 group-hover:border-[#DE89FF]/40">
-                                <img
-                                    alt={certification.institution}
-                                    className="object-fit rounded-full"
-                                    height={48}
-                                    src={certification.icon}
-                                    width={48}
-                                />
-                            </div>
-                            <h4 className="font-bold dark:text-white mb-1 text-sm">
-                                {certification.name}
-                            </h4>
-                            <p className="text-[10px] text-slate-500 uppercase tracking-widest">
-                                {certification.institution}
-                            </p>
-                        </Card>
+                            <Card
+                                className="p-6 w-full rounded-xl flex flex-col items-center text-center hover:border-[#bc13fe66] hover:shadow-[0_0_30px_-10px_rgba(188,19,254,0.2)] hover:bg-[#bc13fe0d] border transition"
+                                key={certification.name}>
+                                <div className="w-16 h-16 mb-4 flex items-center justify-center bg-primary/5 rounded-full border border-primary/20 group-hover:border-[#DE89FF]/40">
+                                    <img
+                                        alt={certification.institution}
+                                        className="object-fit rounded-full"
+                                        height={48}
+                                        src={certification.icon}
+                                        width={48}
+                                    />
+                                </div>
+                                <h4 className="font-bold dark:text-white mb-1 text-sm">
+                                    {certification.name}
+                                </h4>
+                                <p className="text-[10px] text-slate-500 uppercase tracking-widest">
+                                    {certification.institution}
+                                </p>
+                            </Card>
+                        </Link>
                     ))}
                 </div>
             </div>
